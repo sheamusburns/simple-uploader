38ce2ef93bf0aa2193005312e3c801f121309b75
Sheamus Burns <sheamusburns@gmail.com>
2018-03-24 22:39:16 -0400

    add /list to show files

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const http = require('http');
 const fs = require('fs');
-const mp = require('multiparty')
+const mp = require('multiparty');
 
 const server = http.createServer((req, res) => {
 	let path;
 	if (/.+?(?=\?)/.exec(req.url)) {
 		path = /.+?(?=\?)/.exec(req.url)[0];
-	} else path = req.url
+	} else path = req.url;
 
-	if (path == '/upload' && req.method == 'GET') {
+	if (path === '/upload' && req.method === 'GET') {
 		send('./index.html').to(res);
-	} if (path == '/upload' && req.method == 'POST') {
+	} 
+	if (path == '/upload' && req.method == 'POST') {
 		let form = new mp.Form({uploadDir:'uploads'});
 		form.parse(req, function(err, fields, files) {
-      // fs.createReadStream(files.testFile[0].path).pipe(res);
       send('./index.html').to(res);
     });
 	}
+	if (path === '/list' && req.method === 'GET') {
+		console.log('yes')
+		html = "<!DOCTYPE html><html><head></head><body><ul>"
+		fs.readdir('uploads', (err, ls) => {
+			if (err) throw err;
+			ls.forEach(i => {
+				html+= "<li>"+i+"</li>"
+			})
+			html+="</ul></body></html>"
+			// console.log(ls)
+			res.write(html);
+			res.end();
+		});
+	}
+
 });
 
 function send(file) {
